684dce18305dfb91d9f2ea3b22a7d779247a3e84
Maryse_GAHOU <marysegahou@gmail.com>
2024-01-18 08:38:36 +0100

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ import requests
 def number_of_subscribers(subreddit):
     """queries the Reddit API"""
     headers = {
-        "User-Agent": "0x16. API_advanced-e_Maryse"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Single_Parsnip4249)"
         }
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
